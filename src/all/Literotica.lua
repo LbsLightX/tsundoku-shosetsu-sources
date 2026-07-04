f871ae58489382e41df37f4028bf9b3326c3b57d
LbsLightX <shivamkumarking66@gmail.com>
2026-07-04 02:10:36 +0000

    bump(literotica): v1.0.12 index & version

--- src/all/Literotica.lua
+++ src/all/Literotica.lua
@@ -1,4 +1,4 @@
--- {"id":1308639970,"ver":"1.0.11","libVer":"1.0.0","author":"Jobobby04"}
+-- {"id":1308639970,"ver":"1.0.12","libVer":"1.0.0","author":"Jobobby04"}
 
 local baseURL = "https://www.literotica.com"
 local settings = {}
